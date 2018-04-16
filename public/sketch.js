b48f566f218e7cc26e0ded1067bb68f5f5de584b
John <john.stringer.martin@gmail.com>
2018-04-16 15:34:20 +0100

    including sketch

--- public/sketch.js
+++ public/sketch.js
@@ -1,26 +1,48 @@
-var socket;
+let canvas;
+let greenLevel;
+let redLevel;
+let socket;
+let incomingData = 0;
+let interpIncomingData = 0;
+let col = 0;
 
 function setup() {
-createCanvas(200,200)
+  //sets the canvas to the height and with of the window
+  canvas = createCanvas(windowWidth, windowHeight);
+  canvas.position(0,0);
+  //place the positioning of the sketch as the lowest layer in case I want to have any layers on top to add
+  canvas.style('z-index', '-1');
 
-socket = io.connect('http://localhost:3000')
-socket.on('mouse', newDrawing);
-}
-
-function newDrawing(data) {
-  fill(255,0,100);
-  ellipse(data.x, data.y, 60, 60);
+  socket = io();
+  socket.on('ServerToClient', socketEvents);
+  //socket = io.connect('https://iot-network.herokuapp.com:3000');
+  socket = io.connect('http://localhost:3000');
+  socket.on('ServerToClient', socketEvents);
 }
 
 function draw() {
-  background(51);
-  ellipse(mouseX, mouseY, 60, 60);
-  console.log('Sending: ' + mouseX + ',' + mouseY);
+  background(0);
+  var co2 = 1;
 
-  var data = {
-    x: mouseX,
-    y: mouseY
+  console.log(incomingData);
+
+  if (co2 > 2.5) {
+    background(255, 0, 0);
+  } else if (co2 > 1) {
+    //greenLevel = 255 - ceil(co2 * 100);
+    greenLevel = 255 - ceil(map(co2, 1, 2.5, 0, 255));
+    background(255, greenLevel, 0);
+  } else {
+    //redLevel =  100 + ceil(co2 * 100);
+    redLevel = ceil(map(co2, 0, 1, 0, 255));
+    background(redLevel, 255, 0);
   }
+  //creates and circle in the centre of the canvas that dynamically changes size based on co2
+  ellipse(width/2, height/2, co2*100);
+}
 
-  socket.emit('mouse', data);
+function socketEvents(data){
+  incomingData = data;
+  incomingData = map(incomingData, 0, 100, 0, 255);
+  console.log(data);
 }
--- server.js
+++ server.js
@@ -1,31 +1,49 @@
+var mqtt = require('mqtt')
+var MQTT_TOPIC = "datacloudNR";
+var MQTT_ADDR = "mqtt://broker.i-dat.org:80";
+var MQTT_PORT = 80;
+
+var client  = mqtt.connect(MQTT_ADDR);
+
 var express = require('express');
 var socket = require('socket.io');
 
+//store the express functions to var app
 var app = express();
-var server = app.listen(process.env.PORT || 3000, listen);
+//Create a server on localhost:3000
+var server = app.listen(process.env.PORT || 3000);
+app.use(express.static('public'));
+console.log("Node is running on port 3000...");
 
+//assign the server to the socket
 var io = socket(server);
 
-function listen() {
-  var host = server.address().address;
-  var port = server.address().port;
-  console.log('Example app listening at http://' + host + ':' + port);
-}
+//MQTT
+client.on('connect', function() {
+    client.subscribe("datacloudNR/co2", { qos: 1 });
+    //client1.publish(MQTT_TOPIC, '000');
+});
 
-app.use(express.static('public'));
+client.on('message', function (topic, message) {
+    let getNum = parseInt(message.toString());
+    console.log("CO2: "+getNum);
+    io.sockets.emit('ServerToClient', getNum);
+    //io.sockets.on('connection', function (socket) {
+    //  socket.broadcast.emit(getNum);
+    //});
+    //client.end();
+});
 
-console.log("Socket server is running");
+/*
+client.on('error', function(){
+    console.log("ERROR")
+    client.end()
+})
+client.on('offline', function() {
+    console.log("offline");
+});
 
-io.sockets.on('connection', newConnection);
-
-function newConnection(socket) {
-  console.log('new connection: ' + socket.id);
-
-  socket.on('mouse', mouseMsg);
-
-  function mouseMsg(data){
-    socket.broadcast.emit('mouse', data);
-    // io.sockets.emit('mouse', data);
-    console.log(data);
-  }
-}
+client.on('reconnect', function() {
+    console.log("reconnect");
+});
+*/
